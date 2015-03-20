c71812f5f6d01084b798606761099b0257090701
301 <301@223.194.169.99>
2015-03-20 12:48:14 +0900

    두번째.

--- 2.FirstProject/src/exam/first/HelloServlet.java
+++ 2.FirstProject/src/exam/first/HelloServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class HelloServlet
  */
-@WebServlet("/Hello")
+@WebServlet("/HelloServlet") //@이노테이션("/URL 맵핑")
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -20,6 +20,7 @@ public class HelloServlet extends HttpServlet {
      * Default constructor. 
      */
     public HelloServlet() {
+    	super();
         // TODO Auto-generated constructor stub
     }
 
@@ -29,8 +30,20 @@ public class HelloServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
+		//한글처리
+		response.setContentType("text/html;charset=UTF-8");
+		request.setCharacterEncoding("UTF-8"); 
 		PrintWriter out = response.getWriter();
-		out.print("Hello");
+		
+		String name = request.getParameter("name");// test2와 같아야함.
+		String id = request.getParameter("ID");
+		String pwd = request.getParameter("pwd");
+		
+		out.print("Hello-GET<br>");
+		out.print("이름 : "+name+"<br>");
+		out.print(id+"<br>");
+		out.print(pwd+"<br>");
+		
 		
 	}
 
@@ -39,6 +52,21 @@ public class HelloServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
+		response.setContentType("text/html;charset = UTF-8"); //한글을 지원 해줌
+		request.setCharacterEncoding("UTF-8"); 				  //요청시 한글을 지원 해줌
+		
+		PrintWriter out = response.getWriter();
+		
+		out.print("Hello-POST<br>");
+		
+		String name = request.getParameter("name");
+		String id = request.getParameter("ID");
+		String pwd = request.getParameter("pwd");
+		
+		out.print("이름 : "+name+"<br>");
+		out.print(id+"<br>");
+		out.print(pwd+"<br");
 	}
 
 }
